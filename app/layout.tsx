58529c3f5e66fb5e2a728be26207a37bb25b5827
life0080 <115698510+life0080@users.noreply.github.com>
2023-11-09 16:02:50 +0800

    Update layout.tsx
    修改标题名称为sivco Assistant

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { getClientConfig } from "./config/client";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "Siveco Assistant",
+  description: "Your personal ChatGPT Assistant.",
   viewport: {
     width: "device-width",
     initialScale: 1,
